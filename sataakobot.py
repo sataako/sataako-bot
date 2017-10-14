f0c7afe81b79d6ac3b0e360ba34b02344e1e5a17
ikuisma <ikuisma@Ilkkas-MacBook-Pro.local>
2017-10-14 18:36:11 +0300

    CLI command for running app locally using polling and with an optional argument using a webhook on Heroku.

--- sataakobot.py
+++ sataakobot.py
@@ -1,18 +1,29 @@
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import os
+import argparse
+
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
+logger = logging.getLogger('sataakobot')
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--deploy-to-heroku',
+                    dest='deploy_to_heroku',
+                    action='store_true',
+                    help='Add this argument to deploy the bot on Heroku. ')
+parser.set_defaults(deploy_to_heroku=False)
+
+TELEGRAM_API_TOKEN = os.environ.get('TELEGRAM_API_TOKEN')
+
+if not TELEGRAM_API_TOKEN:
+    for key in os.environ:
+        print(key)
+    raise EnvironmentError('You must set a Telegram API token key environment variable. ')
 
 
-TELEGRAM_API_KEY = os.environ.get('TELEGRAM_API_KEY')
-
-if not TELEGRAM_API_KEY:
-    raise EnvironmentError('You must set a Telegram API key environment variable. ')
-
-
-updater = Updater(token=TELEGRAM_API_KEY)
+updater = Updater(token=TELEGRAM_API_TOKEN)
 dispatcher = updater.dispatcher
 
 
@@ -36,6 +47,29 @@ dispatcher.add_handler(start_handler)
 dispatcher.add_handler(echo_handler)
 dispatcher.add_handler(unknown_handler)
 
-if __name__ == '__main__':
+
+def start_heroku_webhook():
+    """ Starts the bot on Heroku using a webhook. """
+    heroku_url = os.environ.get('SERVER_URL_HEROKU')
+    port = int(os.environ.get('PORT', '5000'))
+    updater.start_webhook(listen="0.0.0.0",
+                          port=port,
+                          url_path=TELEGRAM_API_TOKEN)
+    updater.bot.set_webhook(heroku_url + TELEGRAM_API_TOKEN)
+    updater.idle()
+
+
+def start_local_polling():
+    """ Starts the bot locally by using polling. """
     updater.start_polling()
     updater.idle()
+
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    if args.deploy_to_heroku:
+        logger.info('Running bot in Heroku using a webhook. ')
+        start_heroku_webhook()
+    else:
+        logger.info('Running bot locally using polling. ')
+        start_local_polling()
